1a8ad4cbed29269031f56668e97f3ed84d5f821f
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-10-15 12:42:31 +0200

    P4PU-991 added AuthAccessTokenRetrieverTest

--- src/test/java/it/gov/pagopa/arc/connector/auth/service/AuthAccessTokenRetrieverTest.java
+++ src/test/java/it/gov/pagopa/arc/connector/auth/service/AuthAccessTokenRetrieverTest.java
@@ -3,15 +3,21 @@ package it.gov.pagopa.arc.connector.auth.service;
 import it.gov.pagopa.arc.connector.auth.client.AuthnClient;
 import it.gov.pagopa.arc.utils.TestUtils;
 import it.gov.pagopa.pu.auth.dto.generated.AccessToken;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 import uk.co.jemos.podam.api.PodamFactory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 @ExtendWith(MockitoExtension.class)
 class AuthAccessTokenRetrieverTest {
@@ -28,7 +34,7 @@ class AuthAccessTokenRetrieverTest {
     }
 
     @Test
-    void givenParamsWhenGetAccessToken(){
+    void givenParamsWhenGetAccessTokenThenReturnAccessToken(){
         AccessToken accessToken = podamFactory.manufacturePojo(AccessToken.class);
         String grantType = "client_credentials";
         String scope = "openid";
@@ -41,5 +47,26 @@ class AuthAccessTokenRetrieverTest {
 
         assertNotNull(result);
         assertSame(accessToken, result);
+    }
+
+    @Test
+    void givenExistingValidTokenWhenGetAccessTokenThenReturnCachedToken() {
+        // given
+        AccessToken cachedToken = podamFactory.manufacturePojo(AccessToken.class);
+        LocalDateTime expiration = LocalDateTime.now().plusMinutes(10);
+
+        Map<String, Pair<LocalDateTime, AccessToken>> tokenMap =
+                (Map<String, Pair<LocalDateTime, AccessToken>>)
+                        ReflectionTestUtils.getField(authAccessTokenRetriever, "clientId2accessTokensMap");
+
+        tokenMap.put("piattaforma-unitaria_", Pair.of(expiration, cachedToken));
+
+        // when
+        AccessToken result = authAccessTokenRetriever.getAccessToken();
+
+        // then
+        assertNotNull(result);
+        assertSame(cachedToken, result);
+        Mockito.verifyNoInteractions(authnClientMock);
     }
 }
