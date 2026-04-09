20a62252cf801cc512a98a2806d827860fc5ce8f
antonio.torre <antonio.torre@pagopa.it>
2026-04-09 19:57:47 +0200

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ import java.util.*
 
 plugins {
     java
-    id("org.springframework.boot") version "4.0.3"
+    id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
     jacoco
     id("org.sonarqube") version "7.2.3.7755"
     id("com.github.ben-manes.versions") version "0.53.0"
-    id("org.openapi.generator") version "7.20.0"
+    id("org.openapi.generator") version "7.21.0"
     id("org.ajoberstar.grgit") version "5.3.2"
     id("com.gorylenko.gradle-git-properties") version "2.5.7"
-    id("com.github.jk1.dependency-license-report") version "3.1.1"
+    id("com.github.jk1.dependency-license-report") version "3.1.2"
 }
 
 group = "it.gov.pagopa"
@@ -49,22 +49,21 @@ repositories {
 
 val springDocOpenApiVersion = "3.0.2"
 val janinoVersion = "3.1.12"
-val openApiToolsVersion = "0.2.9"
+val openApiToolsVersion = "0.2.10"
 val wiremockVersion = "3.13.2"
 val javaJwtVersion = "4.5.1"
 val jwksRsaVersion = "0.23.0"
 val mapStructVersion = "1.6.3"
-val micrometerVersion = "1.6.3"
+val micrometerVersion = "1.6.4"
 val commonsLang3Version = "3.20.0"
 val commonsFileUploadVersion = "1.6.0"
 val httpClientVersion = "5.6"
-val httpCoreVersion = "5.4.1"
+val httpCoreVersion = "5.4.2"
 val podamVersion = "8.0.2.RELEASE"
 val bouncycastleVersion = "1.83"
 
 // fix cve
-val jackson2CoreVersion = "2.21.1"
-val jackson3CoreVersion = "3.1.0"
+val jackson3CoreVersion = "3.1.1"
 
 val springCloudDepsVersion = "2025.1.1"
 
@@ -101,7 +100,6 @@ dependencies {
 
     // CVE fix
     implementation("tools.jackson.core:jackson-core:${jackson3CoreVersion}")
-    implementation("com.fasterxml.jackson.core:jackson-core:${jackson2CoreVersion}")
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
